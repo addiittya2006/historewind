fd70fff927e019ff89e4edce425db8319bd4f422
Aditya <addiittya2006@gmail.com>
2017-03-22 09:04:04 +0530

    overscroll fix

--- src/scripts/controllers/toolbar-ctrl.js
+++ src/scripts/controllers/toolbar-ctrl.js
@@ -9,15 +9,21 @@ angular.module('toolbarCtrl', [])
 				templateUrl: './src/templates/colorDialog.html',
 				parent: angular.element(document.body),
 				clickOutsideToClose:true,
-    			fullscreen: $scope.customFullscreen
+				fullscreen: $scope.customFullscreen
 			});
 			function colorDialog ($scope, datatopass) { 
-			    // $scope.projectData = datatopass;  
 				$scope.changeBackground = function(color) {
-					console.log('enter' + color);
-					angular.element(document.querySelector('html')).css('background', color);
+					// angular.element($0).css('background-color', color);
+					// console.log($scope.html);
+					// var appElement = document.querySelector('[ng-app=app]');
+					// var appScope = angular.element(appElement).scope();
+					// var controllerScope = appScope.$$childHead;
+					// console.log(controllerScope);
+					// angular.element(document.querySelector('html')).css('background', color);
+					// angular.element(document.querySelector('html'))[0].style.backgroundColor = "red";
+					// console.log(angular.element(document.querySelector('[ng-app=app]')).scope());
 					angular.element(document.querySelector('.toolbar')).css('background', color);
-					angular.element(document.querySelector('.gallary')).css('background', color);
+					angular.element(document.querySelector('.gallery')).css('background', color);
 
 				}
 			}
